f154224339b71c2bb19881f9ec57e72e092f82f0
AWLR <lehrelex@gmail.com>
2023-12-16 13:02:19 +0800

    Submit

---
+++ day13task2.php
@@ -0,0 +1,26 @@
+<?php
+echo "Day 13 Task 2 <br>";
+
+$dayOfWeek = "Saturday";
+
+switch($dayOfWeek){
+
+    case "Monday":
+    echo"It's a weekday <br>"; break;
+
+    case "Tuesday":
+    echo"It's a weekday <br>"; break;
+
+    case "Wednesday":
+    echo"It's a weekday <br>"; break;
+
+    case "Thursday":
+    echo"It's a weekday <br>"; break;
+
+    case "Friday":
+    echo"It's a weekday <br>"; break;
+
+    default:
+    echo"It's the weekend <br>"; break;
+}
+?>
